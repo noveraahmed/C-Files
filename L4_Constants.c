57e1e01dfce39bba5ebbba801d6982f066b8909f
noveraahmed <46060264+noveraahmed@users.noreply.github.com>
2021-12-25 04:00:37 +0000

    Added arithmetic operators, changed name of files into lessons

--- L4_Constants.c
+++ L4_Constants.c
@@ -7,7 +7,7 @@ int main(){
 // if we said pi = another number, this will alter result of program
 // turn the variable into a constant to prevent it changing the value of the constant 
 
-const float PI = 3.14159
+const float PI = 3.14159;
 //make letters uppercase and add "const"
 
 printf("%f", PI);
---
+++ L5_ArithmeticOperators.c
@@ -0,0 +1,30 @@
+#include <stdio.h> 
+
+int main(){
+
+// arithmetic operators
+/*  + 
+    -
+    * (multiplication)
+    / (division)
+    % (modulus)
+    ++ increment
+    -- decrement
+
+    */
+
+int x = 5;
+int y = 2;
+
+int z = x / y; //returns int
+float z = x / y; //returns float
+
+float z = x / (float) y; 
+
+int z = x % y; // remainder of division
+
+x++; // returns 6
+y--; // returns 1 
+
+return 0;
+}
