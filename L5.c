32c15f987d68b416b6a3500480fbfae8171b4bbf
noveraahmed <46060264+noveraahmed@users.noreply.github.com>
2021-12-25 23:05:47 +0000

    Added Augmented assignment operators, modified read.me

--- L5.c
+++ L5.c
@@ -26,5 +26,23 @@ int z = x % y; // remainder of division
 x++; // returns 6
 y--; // returns 1 
 
+//////////////////////////////////////////////////////////////
+
+// augmented assignment operators = used to replace a statement where an operator takes a variable as one of its arguments and then assigns the result back to the same variable 
+
+// x = x+1
+// x+=1 
+
+int n = 10;
+
+// n = n + 2;
+// n += 2
+
+//n = n-3;
+//n -= 3;
+
+//n = n*4;
+//n*=4;
+
 return 0;
 }
--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # C-Files
  
  Just template files being used to learn C and github
+
+Directory
+
+ L1 - Variables 
+
+ L2 - Data Types
+
+ L3 - Format Specifiers
+
+ L4 - Constants
+
+ L5 - Arithmetic Operators, Augmented Assignment Operators
